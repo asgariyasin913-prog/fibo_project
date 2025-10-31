c1dc8d4392a81c3da56114f650fe253b6588cb24
asgariyasin-prog <Asgariyasin913@gmail.com>
2025-10-31 16:47:39 +0330

    done3

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ print(result)
 print(fib(number))
 print(fib(number))
 
-
 def fib(number:int):
+    if number<=1:
+        return number
+    else:
+        return fib(number-1)+fib(number-2)
+
+number=6
+result=fib(number)
+print(result)
+
